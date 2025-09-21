45e28e893970627372756c8eb0f61597c76d0cae
Ayush Kumar <ak433778@gmail.com>
2025-09-21 19:44:38 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -8,10 +8,16 @@ app.use(cors());
 
 app.get("/api/reddit", async (req, res) => {
   try {
-    const response = await fetch("https://www.reddit.com/r/reactjs.json");
+    const response = await fetch("https://www.reddit.com/r/reactjs.json", {
+      headers: {
+        "User-Agent": "node:reddit.fetcher:v1.0 (by /u/ak433778)",
+      },
+    });
+
     const data = await response.json();
     res.json(data);
   } catch (err) {
+    console.error(err); // log actual error
     res.status(500).json({ error: "Failed to fetch Reddit data" });
   }
 });
